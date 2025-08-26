f66e0b3e05f83a9e7da38a9d0b209270b9734b6a
Aaron Freimark <aaron@tekserve.com>
2025-08-26 11:59:00 -0400

    Initial Commit

---
+++ OIDC Tester/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OIDC Tester
+//
+//  Created by Aaron Freimark on 8/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OIDC Tester/OIDC_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OIDC_TesterApp.swift
+//  OIDC Tester
+//
+//  Created by Aaron Freimark on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct OIDC_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
